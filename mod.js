df38d665a7a2a75d0e0bba76bd19c9f49a98e74b
Urbigmumz <140040348+Urbigmumz-dev@users.noreply.github.com>
2024-02-27 11:29:40 -0600

    Update mod.js

--- mod.js
+++ mod.js
@@ -1,76 +1,35 @@
-var dropdowns = [];
-var selectedDropdown = null;
-var selectedButton = null;
-
-function Dropdown(x, y, label, buttons) {
-    this.x = x;
-    this.y = y;
-    this.label = label;
-    this.buttons = buttons;
-    this.expanded = false;
+// Function to draw line from center of screen to player
+function drawLineToPlayer(player) {
+    var renderManager = RenderManager.instance;
+    var scaledRes = new ScaledResolution(mc);
+    var centerX = scaledRes.getScaledWidth() / 2;
+    var centerY = scaledRes.getScaledHeight() / 2;
+    
+    var playerPos = new Vec3(player.posX, player.posY, player.posZ);
+    var screenPos = renderManager.getRenderViewEntity().getLook(1.0).normalize().subtract(playerPos).normalize();
+    
+    // Adjust line length to be within render distance
+    screenPos = screenPos.scale(mc.gameSettings.renderDistanceChunks * 16);
+    
+    // Draw line
+    renderManager.drawLine(centerX, centerY, centerX + screenPos.xCoord, centerY + screenPos.yCoord, 2, 0x00FF00);
 }
 
-Dropdown.prototype.draw = function() {
-    // Draw dropdown label
-    ModAPI.drawRect(this.x, this.y, 100, 10, 0x40000000); // Gray background for label
-    ModAPI.drawStringWithShadow({msg: this.label, x: this.x + 2, y: this.y + 2, color: -1});
-
-    if (this.expanded) {
-        // Draw expanded options
-        for (var i = 0; i < this.buttons.length; i++) {
-            var buttonColor = selectedButton === this.buttons[i] ? 0x8000FF00 : 0x80CCCCCC; // Green if selected, gray if not
-            ModAPI.drawRect(this.x, this.y + 10 * (i + 1), 100, 10, buttonColor); // Button background
-            ModAPI.drawStringWithShadow({msg: this.buttons[i], x: this.x + 2, y: this.y + 10 * (i + 1) + 2, color: -1});
+// Function to draw lines to all players in render distance
+function drawLinesToPlayers() {
+    var world = mc.theWorld;
+    var renderManager = RenderManager.instance;
+    var players = world.playerEntities;
+    
+    for (var i = 0; i < players.length; i++) {
+        var player = players[i];
+        if (player !== mc.thePlayer) {
+            drawLineToPlayer(player);
         }
     }
-};
-
-Dropdown.prototype.handleClick = function(mx, my) {
-    if (mx >= this.x && mx <= this.x + 100 && my >= this.y && my <= this.y + 10) {
-        this.expanded = !this.expanded;
-    } else if (this.expanded) {
-        for (var i = 0; i < this.buttons.length; i++) {
-            if (mx >= this.x && mx <= this.x + 100 && my >= this.y + 10 * (i + 1) && my <= this.y + 10 * (i + 2)) {
-                selectedButton = this.buttons[i];
-                break;
-            }
-        }
-    }
-};
-
-function executeAction(action) {
-    // Execute selected action
-    // Example: changeSettings(action);
 }
 
-// Create dropdowns
-dropdowns.push(new Dropdown(50, 100, "Dropdown 1", ["Option 1", "Option 2", "Option 3"]));
-dropdowns.push(new Dropdown(50, 120, "Dropdown 2", ["Option A", "Option B", "Option C"]));
-
-var customMenuOpen = false;
-
-function CustomGuiMenu() {
-    this.drawScreen = function() {
-        // Draw custom menu UI here
-        ModAPI.drawRect(50, 50, 200, 200, 0x80000000); // Transparent background
-        // Draw dropdowns
-        for (var i = 0; i < dropdowns.length; i++) {
-            dropdowns[i].draw();
-        }
-    };
-
-    this.handleInput = function(mx, my, action) {
-        // Handle input events here
-        if (action === "click") {
-            for (var i = 0; i < dropdowns.length; i++) {
-                dropdowns[i].handleClick(mx, my);
-            }
-        }
-    };
-
-    this.onClose = function() {
-        customMenuOpen = false;
-        selectedDropdown = null;
-        selectedButton = null;
-    };
-}
+// Event listener to draw lines every frame
+ModAPI.addEventListener('drawhud', function() {
+    drawLinesToPlayers();
+});
