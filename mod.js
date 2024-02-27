fd7b6b6b96671816dd3b63145438eb8192e8aedc
Urbigmumz <140040348+Urbigmumz-dev@users.noreply.github.com>
2024-02-27 11:24:15 -0600

    Update mod.js

--- mod.js
+++ mod.js
@@ -1,20 +1,26 @@
 var dropdowns = [];
+var selectedDropdown = null;
+var selectedButton = null;
 
-function Dropdown(x, y, label, options) {
+function Dropdown(x, y, label, buttons) {
     this.x = x;
     this.y = y;
     this.label = label;
-    this.options = options;
+    this.buttons = buttons;
     this.expanded = false;
 }
 
 Dropdown.prototype.draw = function() {
     // Draw dropdown label
-    ModAPI.drawStringWithShadow({msg: this.label, x: this.x, y: this.y, color: -1});
+    ModAPI.drawRect(this.x, this.y, 100, 10, 0x40000000); // Gray background for label
+    ModAPI.drawStringWithShadow({msg: this.label, x: this.x + 2, y: this.y + 2, color: -1});
+
     if (this.expanded) {
         // Draw expanded options
-        for (var i = 0; i < this.options.length; i++) {
-            ModAPI.drawStringWithShadow({msg: this.options[i], x: this.x, y: this.y + 10 * (i + 1), color: -1});
+        for (var i = 0; i < this.buttons.length; i++) {
+            var buttonColor = selectedButton === this.buttons[i] ? 0x8000FF00 : 0x80CCCCCC; // Green if selected, gray if not
+            ModAPI.drawRect(this.x, this.y + 10 * (i + 1), 100, 10, buttonColor); // Button background
+            ModAPI.drawStringWithShadow({msg: this.buttons[i], x: this.x + 2, y: this.y + 10 * (i + 1) + 2, color: -1});
         }
     }
 };
@@ -23,20 +29,18 @@ Dropdown.prototype.handleClick = function(mx, my) {
     if (mx >= this.x && mx <= this.x + 100 && my >= this.y && my <= this.y + 10) {
         this.expanded = !this.expanded;
     } else if (this.expanded) {
-        for (var i = 0; i < this.options.length; i++) {
+        for (var i = 0; i < this.buttons.length; i++) {
             if (mx >= this.x && mx <= this.x + 100 && my >= this.y + 10 * (i + 1) && my <= this.y + 10 * (i + 2)) {
-                // Handle option click
-                // Example: executeOption(this.options[i]);
-                this.expanded = false;
+                selectedButton = this.buttons[i];
                 break;
             }
         }
     }
 };
 
-function executeOption(option) {
-    // Execute selected option action
-    // Example: changeSettings(option);
+function executeAction(action) {
+    // Execute selected action
+    // Example: changeSettings(action);
 }
 
 // Create dropdowns
@@ -66,5 +70,7 @@ function CustomGuiMenu() {
 
     this.onClose = function() {
         customMenuOpen = false;
+        selectedDropdown = null;
+        selectedButton = null;
     };
 }
